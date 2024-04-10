76f8846e32c3d960a72b50c97ed4c00eca5d61c4
MonkeyKao <sunnylovemonkey@gmail.com>
2024-04-10 17:29:29 +0800

    feat(ts): 利用變數記錄產品資訊

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-let producting = "圖像 Angular 開發入門 第二版";
-console.log(producting);
+let productId = 1;
+console.log("Product Id=", productId);
 
-const productCss = "金魚都能懂的 CSS 必學屬性";
-console.log(productCss);
+const productName = "圖像 Angular 開發入門 第二版";
+console.log("Product =", productName);
+
+let isShow = true;
+console.log("Is Show=", isShow);
