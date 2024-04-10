91c569326c417de5166a32f9fbd3deb707c28de4
MonkeyKao <sunnylovemonkey@gmail.com>
2024-04-10 18:56:49 +0800

    feat(ts): 利用陣列記錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 11)
-);
+const product = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 11)
+  ),
+  new Product(
+    2,
+    "金魚都能懂得 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 11)
+  ),
+];
+
 console.log(product);
 console.table(product);
