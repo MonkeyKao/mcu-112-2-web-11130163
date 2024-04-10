664465c8570d78b218f663abd46560abc8424d4c
MonkeyKao <sunnylovemonkey@gmail.com>
2024-04-10 18:48:56 +0800

    refactor(ts): 變更產品建構式

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Product } from "./product";
-const product = {
-  id: 1,
-  name: "圖像 Angular 開發入門 第二版",
-  company: "博碩文化",
-  price: 500,
-  isShow: true,
-  creatDate: 2024 - 4 - 10,
-  modifyDate: undefined,
-};
+
+const product = new Product(
+  1,
+  "圖像 Angular 開發入門 第二版",
+  "博碩文化",
+  500,
+  true,
+  new Date(2024, 3, 11)
+);
 console.log(product);
 console.table(product);
