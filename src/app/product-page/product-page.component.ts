df2989c1e609219c5506de6287f1f1c518960e4e
MonkeyKao <sunnylovemonkey@gmail.com>
2024-06-19 07:51:13 +0800

    feat(product-service): 變更產品資料新增，修改與刪除回傳型別

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -31,7 +31,7 @@ export class ProductPageComponent implements OnInit {
       createDate: new Date(),
       price: 10000,
     });
-    this.productService.add(product);
+    this.productService.add(product).subscribe();
   }
 
   onEdit(product: Product): void {
@@ -39,7 +39,7 @@ export class ProductPageComponent implements OnInit {
   }
 
   onRemove({ id }: Product): void {
-    this.productService.remove(id);
+    this.productService.remove(id).subscribe();
   }
 
   onView(product: Product): void {
