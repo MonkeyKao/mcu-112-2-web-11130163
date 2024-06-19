73a7cde19d6c10ed02026a5a9f8b182806e1525b
MonkeyKao <sunnylovemonkey@gmail.com>
2024-06-19 14:16:36 +0800

    feat(product): 實作新增產品資料至資料庫

--- src/app/services/product-remote.service.ts
+++ src/app/services/product-remote.service.ts
@@ -10,11 +10,16 @@ import { ProductService } from './product.service';
 export class ProductRemoteService extends ProductService {
   private readonly url = 'http://localhost:3000/products';
   private readonly httpClient = inject(HttpClient);
+
   override getById(productId: number): Observable<Product> {
     return this.httpClient.get<Product>(`${this.url}/${productId}`);
   }
 
   override getList(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(this.url);
+  }
+
+  override add(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(this.url, { ...product });
   }
 }
